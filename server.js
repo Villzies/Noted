7185dbac2eca47329b8720737296aa269075f839
Villzies <gemberling.stephan@att.net>
2023-11-22 17:21:45 -0600

    Minor syntax changes

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.get("*", function (req, res) {
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
 
 // GET Route for notes page
 app.get('api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/db/db.json'))
+  res.sendFile(path.join(__dirname, "db/db.json"))
 );
 
 app.listen(PORT, () =>
