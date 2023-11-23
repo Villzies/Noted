58cacaeb967d9562c5a40ea8579f59f0049ff1c9
Villzies <gemberling.stephan@att.net>
2023-11-23 10:55:12 -0600

    minor formatting change to pathing

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(express.static('public'));
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET Route for homepage
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
   })
 
   app.listen(PORT, () =>
